b7565f5f0e846cdfe395c54e628d115feb298db0
TheAdventurousMiner <csantospablos@gmail.com>
2025-07-17 19:07:07 -0400

    Added a bool statement to test if the mouse is near the needle and if statement to run the code if true. Also added comments to explain the code.

--- Assets/Scripts/AmberDetector.cs
+++ Assets/Scripts/AmberDetector.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AmberDetector : MonoBehaviour
 {
+    //Make a public sprite renderer to get access to the sprite in the inspector
     public SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
@@ -15,14 +16,22 @@ public class AmberDetector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //create a new Vector3 to access the mouse position (Input.mousePosition) in the world space (ScreenToWorldPoint) within the main camera (Camera.main)
         Vector3 amberLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        if(amberLocation.x > 1 && amberLocation.y > -2)
+        //use bool statement to run the code if the amber, followed by mouse position, is near the location of the machine in the scene. In this case the machine
+        //is past 1 in the x coordinates in the scene and above -2 in the y coordinates of the scene.
+        bool isAmberNearMachine = amberLocation.x > 1 && amberLocation.y > -2;
+
+        //if bool statement is true, change the colour of the detector to green, if not, change it to red.
+        if (isAmberNearMachine)
         {
+            //changes colour of the sprite to green.
             spriteRenderer.color = Color.green;
         }
         else
         {
+            //changes the colour of the sprite to red.
             spriteRenderer.color = Color.red;
         }
     }
